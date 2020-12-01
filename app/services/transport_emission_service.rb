98e1c021b268b826f37f7c8200208dc2d3db1323
Maarten Bron <maarten@roads2ranking.com>
2020-12-01 20:55:26 +0100

    api-call is working, storing and sending data

--- app/services/transport_emission_service.rb
+++ app/services/transport_emission_service.rb
@@ -2,6 +2,7 @@ class TransportEmissionService
   require "open-uri"
   def initialize(attributes)
     @transport = attributes
+    @uri = "https://api.carbonkit.net/3.6/categories/"
   end
 
   def call
@@ -11,20 +12,16 @@ class TransportEmissionService
   private
 
   def calculate_emissions
-    # if @transport == "walk/bike"
-    #   return 0
-    # elsif @transport == "car"
-    # byebug
-    request = open("https://api.carbonkit.net/3.6/categories/Generic_car_transport/calculation?fuel=average&values.useTypicalDistance=true",
-              "Accept" => "application/json",
-              http_basic_authentication: ["Maarten", "6E@ex37Sj^7tOy4SWf"]).read
-    data = JSON.parse(request)
-    # data = open_url("https://api.carbonkit.net/3.6/categories/Generic_car_transport/calculation?fuel=average&values.useTypicalDistance=true")
-    data["output"]["amounts"].first["value"]
-    # else @transport == "train"
-    #   data = open_url("https://api.carbonkit.net/3.6/categories/Generic_train_transport/calculation?type=underground&values.IsReturn=true&values.UseTypicalDistance=true&values.Journeyfrequency=daily&values.Numberofjourneys=5")
-    #   data["output"]["amounts"].first["value"]
-    # end
+    case @transport
+    when "car"
+      data = open_url("#{@uri}Generic_car_transport/calculation?fuel=average&values.useTypicalDistance=true")
+      data["output"]["amounts"].first["value"]
+    when "train"
+      data = open_url("#{@uri}Generic_train_transport/calculation?type=underground&values.IsReturn=true
+                      &values.UseTypicalDistance=true&values.Journeyfrequency=daily&values.Numberofjourneys=5")
+      data["output"]["amounts"].first["value"]
+    else 0
+    end
   end
 
   def open_url(url)
