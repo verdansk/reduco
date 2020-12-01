dddad70767c1da2bc6d6ab7bd309217f9d45c7b9
Tiago Enes <enes.tiago@gmail.com>
2020-12-01 17:07:06 +0100

    add survey options

--- app/controllers/surveys_controller.rb
+++ app/controllers/surveys_controller.rb
@@ -1,4 +1,11 @@
 class SurveysController < ApplicationController
   def new
+    @user = current_user
+    @users = User.all
+    @transport = Transport.new
+  end
+
+  def create
+    raise
   end
 end
