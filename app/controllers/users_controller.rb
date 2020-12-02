5420bad9b7d3e0de7ef88c3a89a731318e700526
Alena Gritsiuk <alena22292@mail.ru>
2020-12-02 16:19:36 +0100

    Create a logic for profile picture in nav bar. Add gender field into user model. Update sign_in_bottom on the home page

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,6 +2,7 @@ class UsersController < ApplicationController
   def show
     # dashboard
     @user = current_user
+    # @recent_challenges = User_challenges.order(created_at: :desc).take(4)
   end
 
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_12_01_152905) do
+ActiveRecord::Schema.define(version: 2020_12_02_142932) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -74,6 +74,7 @@ ActiveRecord::Schema.define(version: 2020_12_01_152905) do
     t.string "address"
     t.integer "xp"
     t.string "daily_emission"
+    t.string "gender"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
