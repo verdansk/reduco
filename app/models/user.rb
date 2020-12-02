3da6a39214a4c12f2df7bdf7483102204972298b
Tiago Enes <enes.tiago@gmail.com>
2020-12-02 17:10:30 +0100

    created friend table and display friend

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,11 +7,18 @@ class User < ApplicationRecord
   has_many :housings
   has_many :emissions, through: :transports
 
+  has_many :friendships
+  has_many :friends, through: :friendships
+
   validates :first_name, presence: true
   validates :last_name, presence: true
   validates :address, presence: true
 
   def full_name
     first_name + " " + last_name
+  end
+
+  def all_friendships
+    Friendship.where('user_id=? or friend_id=?',self.id,self.id)
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_12_02_105216) do
+ActiveRecord::Schema.define(version: 2020_12_02_144125) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -32,6 +32,15 @@ ActiveRecord::Schema.define(version: 2020_12_02_105216) do
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.index ["relation_type", "relation_id"], name: "index_emissions_on_relation_type_and_relation_id"
+  end
+
+  create_table "friendships", force: :cascade do |t|
+    t.bigint "user_id", null: false
+    t.bigint "friend_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["friend_id"], name: "index_friendships_on_friend_id"
+    t.index ["user_id"], name: "index_friendships_on_user_id"
   end
 
   create_table "housings", force: :cascade do |t|
@@ -87,6 +96,8 @@ ActiveRecord::Schema.define(version: 2020_12_02_105216) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "friendships", "users"
+  add_foreign_key "friendships", "users", column: "friend_id"
   add_foreign_key "housings", "users"
   add_foreign_key "transports", "users"
   add_foreign_key "user_challenges", "challenges"
