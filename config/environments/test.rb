698f46b5e5af9c4ecc12f4d15ae3991d1656172c
Antal Bako <prowlersk8@gmail.com>
2020-12-02 16:02:14 +0100

    404 and 500 pages done

--- config/environments/test.rb
+++ config/environments/test.rb
@@ -24,7 +24,7 @@ Rails.application.configure do
   config.consider_all_requests_local       = true
   config.action_controller.perform_caching = false
   config.cache_store = :null_store
-
+  config.​serve_static_assets = true
   # Raise exceptions instead of rendering exception templates.
   config.action_dispatch.show_exceptions = false
 
