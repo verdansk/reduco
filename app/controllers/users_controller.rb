bcd7ce002352b748b144bea2d651bd9bb36921a6
Maarten Bron <maarten@roads2ranking.com>
2020-12-03 15:57:24 +0100

    set before_action in user show

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,17 +1,29 @@
 class UsersController < ApplicationController
+  before_action :set_user, only: [:show, :show_animal]
+
   def show
-    # dashboard
-    @user = current_user
     @animal = show_animal
-    # @recent_challenges = User_challenges.order(created_at: :desc).take(4)
   end
 
   def accept_challenge
     @user = User.find(params[:user_id])
   end
 
-  def show_animal
+  def search
+    @query = params[:search].downcase
+    @search_users = User.search_by_first_last_name_email(@query)
     @user = current_user
+    render :show
+  end
+
+  def destroy
+    current_user.destroy
+    redirect_to root_path
+  end
+
+  private
+
+  def show_animal
     if @user.daily_emission.to_i < 12_000
       'bird'
     elsif @user.daily_emission.to_i > 12_000 && @user.daily_emission.to_i <= 13_200
@@ -27,18 +39,7 @@ class UsersController < ApplicationController
     end
   end
 
-  def search
-    @query = params[:search].downcase
-    @search_users = User.search_by_first_last_name_email(@query)
-
-    # @query = params[:search].downcase
+  def set_user
     @user = current_user
-    # @search_users = User.where("lower(first_name) like ?", "%#{@query }%")
-    render :show
-  end
-
-  def destroy
-    current_user.destroy
-    redirect_to root_path
   end
 end
