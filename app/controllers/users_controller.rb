27c4abc9e13a0e7dc6561b09fa49500dbc050220
Maarten Bron <maarten@roads2ranking.com>
2020-12-03 15:50:04 +0100

    based on emission value, the animal svg is generated

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,11 +2,29 @@ class UsersController < ApplicationController
   def show
     # dashboard
     @user = current_user
+    @animal = show_animal
     # @recent_challenges = User_challenges.order(created_at: :desc).take(4)
   end
 
   def accept_challenge
     @user = User.find(params[:user_id])
+  end
+
+  def show_animal
+    @user = current_user
+    if @user.daily_emission.to_i < 12_000
+      'bird'
+    elsif @user.daily_emission.to_i > 12_000 && @user.daily_emission.to_i <= 13_200
+      'owl'
+    elsif @user.daily_emission.to_i > 13_300 && @user.daily_emission.to_i <= 14_400
+      'penguin'
+    elsif @user.daily_emission.to_i > 14_400 && @user.daily_emission.to_i <= 15_600
+      'fox'
+    elsif @user.daily_emission.to_i > 15_600 && @user.daily_emission.to_i <= 16_800
+      'lion'
+    else
+      'bear'
+    end
   end
 
   def search
