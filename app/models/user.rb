a52b2cc2621cf8f7583087508ae5c4593d9c0315
Tiago Enes <enes.tiago@gmail.com>
2020-12-03 12:26:16 +0100

    added PG search

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,14 @@
 class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  include PgSearch::Model
+
+  pg_search_scope :search_by_first_last_name_email,
+    against: [:first_name, :last_name],
+    using: {
+      tsearch: { prefix: true} # <-- now `superman batm` will return something!
+    }
+
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
   has_many :transports
