590ed20715cc49f06343404ee0aface852ec63f3
Tiago Enes <enes.tiago@gmail.com>
2020-12-04 16:53:32 +0100

    fixed divs

--- app/models/user_challenge.rb
+++ app/models/user_challenge.rb
@@ -7,14 +7,8 @@ class UserChallenge < ApplicationRecord
   enum status: {non_started: 0, started: 1, finished: 2}
 
   def time_ago
-    # Date.today
-    # self.start_at
-    # self.challenge.duration
-    # .weeks
-    # .day
-    # Date.today + self.challenge.duration.days
-    # complete_challenge_date =
-    time_ago_in_words(self.start_at + self.challenge.duration.days)
+
+    time_ago_in_words(self.start_at + self.challenge.duration.days+1)
 
   end
 
