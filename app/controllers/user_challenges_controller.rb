d7cb3b719742604e3fe0fcbfb0f2bcd920fc53b9
Alena Gritsiuk <alena22292@mail.ru>
2020-12-04 15:18:00 +0100

    Style

--- app/controllers/user_challenges_controller.rb
+++ app/controllers/user_challenges_controller.rb
@@ -21,6 +21,7 @@ class UserChallengesController < ApplicationController
     user_challenge = UserChallenge.find(params[:id])
     user_challenge.completed = true
     user_challenge.status = "finished"
+
     user_challenge.user.xp += user_challenge.challenge.xp
     user_challenge.user.save
     user_challenge.save
