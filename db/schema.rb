be04ced203308885c5429a9616093ee9002cdbfc
Tiago Enes <enes.tiago@gmail.com>
2020-12-07 15:27:33 +0100

    added a bell ringing when a new notification is received

--- db/schema.rb
+++ db/schema.rb
@@ -135,7 +135,6 @@ ActiveRecord::Schema.define(version: 2020_12_07_105347) do
     t.integer "xp"
     t.string "daily_emission"
     t.string "gender"
-    t.boolean "guest", default: false
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
